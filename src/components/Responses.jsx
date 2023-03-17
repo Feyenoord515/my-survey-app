3e70065f54d716b0ea19eb4e511563788b10fdb8
Feyenoord515 <n.enriquemolinari@gmail.com>
2023-03-17 00:49:05 -0300

    responsive

--- src/components/Responses.jsx
+++ src/components/Responses.jsx
@@ -16,28 +16,31 @@ const Respuestas = () => {
   }, []);
 
   return (
-    <table className="table-fixed w-full">
-      <thead className="bg-gray-200">
-        <tr>
-          <th className="px-4 py-2 w-1/5 text-left font-semibold">Nombre completo</th>
-          <th className="px-4 py-2 w-1/5 text-left font-semibold">Correo electrónico</th>
-          <th className="px-4 py-2 w-1/5 text-left font-semibold">Fecha de nacimiento</th>
-          <th className="px-4 py-2 w-1/5 text-left font-semibold">País de origen</th>
-          <th className="px-4 py-2 w-1/5 text-left font-semibold">Acepta términos y condiciones</th>
+    <div className="overflow-x-auto">
+  <table className="table-auto w-full">
+    <thead className="bg-gray-200">
+      <tr>
+        <th className="px-4 py-2 text-left font-semibold">Nombre completo</th>
+        <th className="px-4 py-2 text-left font-semibold">Correo electrónico</th>
+        <th className="px-4 py-2 text-left font-semibold">Fecha de nacimiento</th>
+        <th className="px-4 py-2 text-left font-semibold">País de origen</th>
+        <th className="px-4 py-2 text-left font-semibold">Acepta términos y condiciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      {respuestas.map((response) => (
+        <tr key={response.id} className="bg-white border-b">
+          <td className="px-4 py-2">{response.full_name}</td>
+          <td className="px-4 py-2">{response.email}</td>
+          <td className="px-4 py-2">{response.birth_date}</td>
+          <td className="px-4 py-2">{response.country_of_origin}</td>
+          <td className="px-4 py-2">{response.terms_and_conditions ? 'Sí' : 'No'}</td>
         </tr>
-      </thead>
-      <tbody>
-        {respuestas.map((response) => (
-          <tr key={response.id} className="bg-white border-b">
-            <td className="px-4 py-2 w-1/5">{response.full_name}</td>
-            <td className="px-4 py-2 w-1/5">{response.email}</td>
-            <td className="px-4 py-2 w-1/5">{response.birth_date}</td>
-            <td className="px-4 py-2 w-1/5">{response.country_of_origin}</td>
-            <td className="px-4 py-2 w-1/5">{response.terms_and_conditions ? 'Sí' : 'No'}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      ))}
+    </tbody>
+  </table>
+</div>
+
   );
 };
 
